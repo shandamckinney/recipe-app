81b18e87179ec95fba3929d2ea32dd34c043b2fe
shandamckinney <mckinney.shanda@gmail.com>
2021-01-04 10:54:09 -0600

    procfile

--- server/index.js
+++ server/index.js
@@ -7,8 +7,9 @@ var db = require('./database');
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
-
 const app = express();
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -21,10 +22,7 @@ if (ENV === 'production') {
   });
 }
 
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!`);
-});
+app.listen(PORT);
 
 db.query('SELECT NOW()', (err, res) => {
     if (err.error)
